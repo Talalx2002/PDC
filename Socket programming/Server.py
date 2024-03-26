4a4e8cc5a6dd6ad6ad04ee3546d227a8d5784519
Talal Khan <FA20-BCS-029@cuiatd.edu.pk>
2024-03-26 08:39:24 +0500

    LAb 1 socket programming

--- Socket programming/Server.py
+++ Socket programming/Server.py
@@ -1,3 +1,10 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1027))
+s.listen(5)
+print("Server is listening...") 
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established!")
+    clientsocket.send(bytes("Socket programming in python ", "utf-8"))
+    clientsocket.close()
