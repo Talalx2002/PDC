b331dcf2484ba4ffdb590139c412339211c6eeea
Talal Khan <FA20-BCS-029@cuiatd.edu.pk>
2024-04-02 09:13:43 +0500

    socket with multi-threading

--- Socket programming/Server.py
+++ Socket programming/Server.py
@@ -3,8 +3,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1027))
 s.listen(5)
 print("Server is listening...") 
+
+input = input("Enter a message: ")  
+
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Socket programming in python ", "utf-8"))
+    clientsocket.send(bytes(input, "utf-8"))
     clientsocket.close()
