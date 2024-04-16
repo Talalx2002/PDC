875f63f892ebcb2039293c0aa363ea5fc475ff23
Talal Khan <FA20-BCS-029@cuiatd.edu.pk>
2024-04-16 10:32:23 +0500

    multi processing function chainges

--- Parallel Processing/pool_class.py
+++ Parallel Processing/pool_class.py
@@ -1,11 +1,15 @@
 from multiprocessing import Pool 
-import time 
+import time
 
 def f(x): 
     return x*x 
 
 def z(x, y): 
     return x*y 
+
+def y(matrix_tuple):
+    matrix, x = matrix_tuple
+    return matrix * x
 
 if __name__ == '__main__': 
     start_time = time.time()  # Start the timer
@@ -16,19 +20,24 @@ if __name__ == '__main__':
     print("Execution time with apply:", end_time - start_time, "seconds")
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
+    matrix = [[1, 2], [3, 4]]  # Example matrix
+
     start_time = time.time()  # Start the timer
-    pool = Pool (processes=4) 
-    pool.map(f, range(30000))
+    pool = Pool(processes=4)
+    for x in range(30000):
+        pool.map(y, ((matrix, x),))  # Note the extra comma to make it a tuple
+    pool.close()
+    pool.join()
     end_time = time.time()  # End the timer
+
     print("Execution time with map:", end_time - start_time, "seconds")
 
-
-
 if __name__ == '__main__': 
     start_time = time.time()  # Start the timer
     pool = Pool (processes=4) 
-    pool.map_async(f, range(30000))
+    for x in range(30000):
+        pool.map_async(y, ((matrix, x),))  # Note the extra comma to make it a tuple
     pool.close()
     pool.join()
     end_time = time.time()  # End the timer
