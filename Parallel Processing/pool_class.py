3fc3f90d9097d4c0d59dd3a3c37c1647c3581e84
Talal Khan <FA20-BCS-029@cuiatd.edu.pk>
2024-04-16 09:18:03 +0500

    pool class function with execution time

---
+++ Parallel Processing/pool_class.py
@@ -0,0 +1,50 @@
+from multiprocessing import Pool 
+import time 
+
+def f(x): 
+    return x*x 
+
+def z(x, y): 
+    return x*y 
+
+if __name__ == '__main__': 
+    start_time = time.time()  # Start the timer
+    pool = Pool (processes=4) 
+    for x in range (30000): 
+        pool.apply(f, (x,))
+    end_time = time.time()  # End the timer
+    print("Execution time with apply:", end_time - start_time, "seconds")
+
+
+if __name__ == '__main__': 
+    start_time = time.time()  # Start the timer
+    pool = Pool (processes=4) 
+    pool.map(f, range(30000))
+    end_time = time.time()  # End the timer
+    print("Execution time with map:", end_time - start_time, "seconds")
+
+
+
+if __name__ == '__main__': 
+    start_time = time.time()  # Start the timer
+    pool = Pool (processes=4) 
+    pool.map_async(f, range(30000))
+    pool.close()
+    pool.join()
+    end_time = time.time()  # End the timer
+    print("Execution time with map_async:", end_time - start_time, "seconds")
+
+
+if __name__ == '__main__': 
+    start_time = time.time()  # Start the timer
+    pool = Pool (processes=4) 
+    pool.starmap(z, [(x, x+1) for x in range(30000)])
+    end_time = time.time()  # End the timer
+    print("Execution time with starmap:", end_time - start_time, "seconds")
+
+    start_time = time.time()  # Start the timer
+    pool.starmap_async(z, [(x, x+1) for x in range(30000)])
+    pool.close()
+    pool.join()
+    end_time = time.time()  # End the timer
+    print("Execution time with starmap_async:", end_time - start_time, "seconds")
